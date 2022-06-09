78a1d52f2d00fbd53847517fb45383f0c36656a1
Gordon Stein <gstein@ltu.edu>
2022-06-09 15:47:13 -0500

    Fix CORS

--- src/RoboScapeSimulatorMainServer.nim
+++ src/RoboScapeSimulatorMainServer.nim
@@ -28,19 +28,22 @@ type
 
 var rooms = newSeq[Room]()
 var servers = newTable[string, Server]()
-var environments = newTable[string, Environment]()
+var environments = newOrderedTable[string, Environment]()
 
 routes:
   get "/server/status":
     # Server statistics
     let maxRoomsSum = if len(servers) > 0:
       toSeq(servers.values()).map(server => server.maxRooms).foldl(a + b) else: 0
-    resp %*{"activeRooms": len(filter(rooms, r => not r.hibernating)),
+    resp Http200, @[("Content-Type", "application/json"), (
+        "Access-Control-Allow-Origin", "*")], $(%*{"activeRooms": len(filter(
+            rooms, r => not r.hibernating)),
         "hibernatingRooms": len(filter(rooms, r => r.hibernating)),
-            "maxRooms": maxRoomsSum}
+            "maxRooms": maxRoomsSum})
   get "/environments/list":
     # List all environments
-    resp %*toSeq(environments.values)
+    resp Http200, @[("Content-Type", "application/json"), (
+        "Access-Control-Allow-Origin", "*")], $(%*toSeq(environments.values))
   get "/rooms/list":
     # List rooms
     var respRooms = rooms
@@ -50,12 +53,16 @@ routes:
       respRooms = rooms.filter(room => room.users.contains(request.params["user"]))
 
     # Output only relevant fields
-    resp %*(respRooms.map(room =>
-        {"id": room.id, "server": room.server,
-            "environment": room.environment}.toTable))
+    resp Http200, @[("Content-Type", "application/json"), (
+        "Access-Control-Allow-Origin", "*")], $(%*(respRooms.map(room => {
+            "id": room.id, "server": room.server,
+            "environment": room.environment}.toTable)))
+
   post "/rooms/create":
     # Request to create a room
-    resp %*{"roomID": "", "server": ""}
+    resp Http200, @[("Content-Type", "application/json"), (
+        "Access-Control-Allow-Origin", "*")], $(%*{"roomID": "", "server": ""})
+
   post "/server/announce":
     # Incoming report from other server
     if not (request.ip in servers):
