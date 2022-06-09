7396d482507c2e65133986c0c745de1fb5aef254
Gordon Stein <gstein@ltu.edu>
2022-06-09 17:20:36 -0500

    Add to room list

--- src/RoboScapeSimulatorMainServer.nim
+++ src/RoboScapeSimulatorMainServer.nim
@@ -10,11 +10,14 @@ onSignal(SIGINT, SIGTERM):
 
 type
   Room = object
-    id: string
-    server: string
+    name: string
+    hasPassword: bool
     environment: string
-    users: seq[string]
-    hibernating: bool
+    lastInteractionTime: string
+    isHibernating: bool
+    creator: string
+    server: Option[string]
+    visitors: Option[seq[string]]
 
   Server = object
     address: string
@@ -37,8 +40,8 @@ routes:
       toSeq(servers.values()).map(server => server.maxRooms).foldl(a + b) else: 0
     resp Http200, @[("Content-Type", "application/json"), (
         "Access-Control-Allow-Origin", "*")], $(%*{"activeRooms": len(filter(
-            rooms, r => not r.hibernating)),
-        "hibernatingRooms": len(filter(rooms, r => r.hibernating)),
+            rooms, r => not r.isHibernating)),
+        "hibernatingRooms": len(filter(rooms, r => r.isHibernating)),
             "maxRooms": maxRoomsSum})
   get "/environments/list":
     # List all environments
@@ -50,12 +53,13 @@ routes:
 
     # Filter to user's rooms
     if request.params.hasKey("user"):
-      respRooms = rooms.filter(room => room.users.contains(request.params["user"]))
+      respRooms = rooms.filter(
+        room => room.visitors.get().contains(request.params["user"]))
 
     # Output only relevant fields
     resp Http200, @[("Content-Type", "application/json"), (
         "Access-Control-Allow-Origin", "*")], $(%*(respRooms.map(room => {
-            "id": room.id, "server": room.server,
+            "id": room.name, "server": room.server.get(),
             "environment": room.environment}.toTable)))
 
   post "/rooms/create":
@@ -65,17 +69,45 @@ routes:
 
   post "/server/announce":
     # Incoming report from other server
-    if not (request.ip in servers):
-      if request.params.hasKey("maxRooms"):
-        servers[request.ip] = Server(address: request.ip,
-            maxRooms: uint16(parseInt(request.params["maxRooms"])))
+    if request.params.hasKey("maxRooms"):
+      servers[request.ip] = Server(address: request.ip,
+          maxRooms: uint16(parseInt(request.params["maxRooms"])))
 
     resp ""
-  post "/server/rooms":
+  put "/server/rooms":
+    # Incoming report from other server
+    if request.ip in servers:
+      if request.params.hasKey("rooms"):
+        # Remove existing entries
+        rooms = rooms.filter(room => room.server.get() != request.ip)
+
+        # Add new entries
+        #for room in
+        echo request.params["rooms"]
+    resp ""
+  delete "/server/rooms":
     # Incoming report from other server
     if request.ip in servers:
       if request.params.hasKey("rooms"):
         echo request.params["rooms"]
+
+    resp ""
+  patch "/server/rooms":
+    # Incoming report from other server
+    if request.ip in servers:
+      if request.params.hasKey("rooms"):
+        try:
+          var parsedRooms = to(parseJson(request.params["rooms"]), seq[Room])
+          rooms = rooms.filter(room => not(room.name in parsedRooms.map(room => room.name)))
+
+          for room in parsedRooms:
+            var tempRoom = room
+            tempRoom.server = some(request.ip)
+            rooms.add(tempRoom)
+
+        except:
+          echo "Error reading rooms"
+
     resp ""
   post "/server/environments":
     # Incoming report from other server
