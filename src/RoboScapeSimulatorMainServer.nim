c86cd4ea1e328b66b1de1f13cb179b57d0314d2a
Gordon Stein <gstein@ltu.edu>
2022-06-11 18:15:42 -0500

    Slight change

--- src/RoboScapeSimulatorMainServer.nim
+++ src/RoboScapeSimulatorMainServer.nim
@@ -68,20 +68,21 @@ when isMainModule:
         withLock roomsLock:
           rooms = rooms.filter(room => not (room.server.get in oldServers))
 
-  discard deadCheck(60 * 1000)
+  discard deadCheck(120 * 1000)
 
 routes:
   get "/server/status":
-    withLock roomsLock:
-      withLock serversLock:
-        # Server statistics
-        let maxRoomsSum = if len(servers) > 0:
+    withLock serversLock:
+      # Server statistics
+      let maxRoomsSum = if len(servers) > 0:
           toSeq(servers.values()).map(server => server.maxRooms).foldl(a + b) else: 0
+
+      withLock roomsLock:
         resp Http200, @[("Content-Type", "application/json"), (
-            "Access-Control-Allow-Origin", "*")], $(%*{"activeRooms": len(
-                filter(rooms, r => not r.isHibernating)),
-            "hibernatingRooms": len(filter(rooms, r => r.isHibernating)),
-                "maxRooms": maxRoomsSum})
+          "Access-Control-Allow-Origin", "*")], $(%*{"activeRooms": len(
+              filter(rooms, r => not r.isHibernating)),
+          "hibernatingRooms": len(filter(rooms, r => r.isHibernating)),
+              "maxRooms": maxRoomsSum})
   get "/environments/list":
     # List all environments
     withLock environmentsLock:
@@ -152,17 +153,17 @@ routes:
       # Incoming report from other server
       if request.params.hasKey("maxRooms"):
         servers[request.ip] = Server(address: request.ip,
-            maxRooms: uint16(parseInt(request.params["maxRooms"])),
-                lastUpdate: now())
+          maxRooms: uint16(parseInt(request.params["maxRooms"])),
+            lastUpdate: now())
 
     resp ""
   put "/server/rooms":
     withLock serversLock:
-      withLock roomsLock:
-        # Incoming report from other server
-        if request.ip in servers:
-          if request.params.hasKey("rooms"):
-            try:
+      # Incoming report from other server
+      if request.ip in servers:
+        if request.params.hasKey("rooms"):
+          try:
+            withLock roomsLock:
               var parsedRooms = to(parseJson(request.params["rooms"]), seq[Room])
               # Remove existing entries
               rooms = rooms.filter(room => room.server.get() != request.ip)
@@ -173,34 +174,35 @@ routes:
                 tempRoom.server = some(request.ip)
                 rooms.add(tempRoom)
 
-              servers[request.ip].lastUpdate = now()
+            servers[request.ip].lastUpdate = now()
 
-            except Exception as e:
-              echo "Error reading rooms: ", e.msg
+          except Exception as e:
+            echo "Error reading rooms: ", e.msg
     resp ""
   delete "/server/rooms":
     withLock serversLock:
-      withLock roomsLock:
-        # Incoming report from other server
-        if request.ip in servers:
-          if request.params.hasKey("rooms"):
-            try:
-              var parsedRooms = to(parseJson(request.params["rooms"]), seq[Room])
+      # Incoming report from other server
+      if request.ip in servers:
+        if request.params.hasKey("rooms"):
+          try:
+            var parsedRooms = to(parseJson(request.params["rooms"]), seq[Room])
+
+            withLock roomsLock:
               rooms = rooms.filter(room => not(room.name in parsedRooms.map(
                   room => room.name)))
-            except Exception as e:
-              echo "Error reading rooms: ", e.msg
+          except Exception as e:
+            echo "Error reading rooms: ", e.msg
 
-            servers[request.ip].lastUpdate = now()
+          servers[request.ip].lastUpdate = now()
 
     resp ""
   patch "/server/rooms":
     withLock serversLock:
-      withLock roomsLock:
-        # Incoming report from other server
-        if request.ip in servers:
-          if request.params.hasKey("rooms"):
-            try:
+      # Incoming report from other server
+      if request.ip in servers:
+        if request.params.hasKey("rooms"):
+          try:
+            withLock roomsLock:
               var parsedRooms = to(parseJson(request.params["rooms"]), seq[Room])
               rooms = rooms.filter(room => not(room.name in parsedRooms.map(
                   room => room.name)))
@@ -210,10 +212,10 @@ routes:
                 tempRoom.server = some(request.ip)
                 rooms.add(tempRoom)
 
-            except Exception as e:
-              echo "Error reading rooms: ", e.msg
+          except Exception as e:
+            echo "Error reading rooms: ", e.msg
 
-          servers[request.ip].lastUpdate = now()
+        servers[request.ip].lastUpdate = now()
 
     resp ""
   post "/server/environments":
